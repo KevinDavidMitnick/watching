ed9414acca6ae4432c66db83ba18c54497bf738f
Guan <enrondg@gmail.com>
2018-03-15 14:07:35 +0800

    Support classification for metric

--- modules/api/app/controller/strategy/strategy_routes.go
+++ modules/api/app/controller/strategy/strategy_routes.go
@@ -39,6 +39,7 @@ func Routes(r *gin.Engine) {
 	met := r.Group("/api/v1/metric")
 	met.Use(utils.AuthSessionMidd)
 	met.GET("default_list", MetricQuery)
+	met.GET("default_metrics", MetricQueryList)
 	sgrp := r.Group("/api/v1/strategyGroup")
 	sgrp.Use(utils.AuthSessionMidd)
 	sgrp.GET("", GetStrategyGroups)
