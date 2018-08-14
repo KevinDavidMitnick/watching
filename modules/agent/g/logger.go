2a0e526ae3c1386b5b495f4319e61eb1da74701e
liucong <liucong@chinac.com>
2018-08-14 17:41:33 +0800

    new agent logger

--- modules/agent/g/logger.go
+++ modules/agent/g/logger.go
@@ -11,14 +11,47 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package g
 
 import (
-	log "github.com/Sirupsen/logrus"
+	"encoding/json"
+	"runtime"
+
+	"fmt"
+	log "github.com/sirupsen/logrus"
 	"os"
+	"strings"
 )
 
+type MyJSONFormatter struct {
+	Time  string `json:"time"`
+	File  string `json:"file"`
+	Line  int    `json:"line"`
+	Level string `json:"level"`
+	Info  string `json:"info"`
+	Msg   string `json:"msg"`
+}
+
+func (f *MyJSONFormatter) Format(entry *log.Entry) ([]byte, error) {
+
+	logrusJF := &(log.JSONFormatter{})
+	bytes, _ := logrusJF.Format(entry)
+
+	json.Unmarshal(bytes, &f)
+	if _, file, no, ok := runtime.Caller(8); ok {
+		f.File = file
+		f.Line = no
+	}
+
+	index := strings.Index(f.Time, "+")
+	times := strings.Replace(f.Time[0:index], "T", " ", 1)
+	str := fmt.Sprintf("[%s] %s %s:%d %s\n", f.Level, times, f.File, f.Line, f.Msg)
+	return []byte(str), nil
+}
+
+func init() {
+	log.SetFormatter(&MyJSONFormatter{})
+}
 func InitLog(level string) (err error) {
 	switch level {
 	case "debug":
