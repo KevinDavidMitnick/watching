5c17963b27cbec7f88ac43029b66feb685c8d83b
liucong <liucong@chinac.com>
2018-08-17 11:09:57 +0800

    fix status of agent

--- modules/agent/cron/flush.go
+++ modules/agent/cron/flush.go
@@ -10,18 +10,11 @@ import (
 )
 
 func updateStoreStatus() {
-	ticker := time.NewTicker(time.Duration(g.Config().Transfer.Interval) * time.Second)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ticker.C:
-			_, err := funcs.GetData(g.Config().Backend.Check)
-			if err == nil {
-				store.UpdateStoreStatus(true)
-			} else {
-				store.UpdateStoreStatus(false)
-			}
-		}
+	_, err := funcs.GetData(g.Config().Backend.Check)
+	if err == nil {
+		store.UpdateStoreStatus(true)
+	} else {
+		store.UpdateStoreStatus(false)
 	}
 }
 
@@ -38,29 +31,24 @@ func consumeStore(queue chan string) {
 }
 
 func cleanStale() {
-	ticker := time.NewTicker(time.Duration(g.Config().Backend.Expire) * time.Second)
-	defer ticker.Stop()
 	for {
-		select {
-		case <-ticker.C:
-			s := store.GetStore()
-			timestamp := time.Now().Unix() - int64(g.Config().Backend.Expire)
-			s.CleanStale(timestamp)
-		}
+		s := store.GetStore()
+		timestamp := time.Now().Unix() - int64(g.Config().Backend.Expire)
+		s.CleanStale(timestamp)
+		time.Sleep(time.Duration(g.Config().Backend.Expire) * time.Second)
 	}
 }
 
 func eatStore(queue chan string) {
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
 	for {
-		select {
-		case <-ticker.C:
-			s := store.GetStore()
-			for data := s.Read(); store.GetStoreStatus() && data != ""; data = s.Read() {
+		s := store.GetStore()
+		updateStoreStatus()
+		if store.GetStoreStatus() {
+			for data := s.Read(); data != ""; data = s.Read() {
 				queue <- data
 			}
 		}
+		time.Sleep(time.Second)
 	}
 }
 
@@ -69,7 +57,6 @@ func FlushStore() {
 		return
 	}
 	queue := make(chan string, g.Config().Transfer.Interval)
-	go updateStoreStatus()
 	go eatStore(queue)
 	go consumeStore(queue)
 	go cleanStale()
